e4740db5a39fadde08a09e3e3737e3b50a419870
mazucanti <daniel.mazucanti.ismart@gmail.com>
2023-05-24 15:26:00 -0300

    placeholder methods and landing page tested in debug mode

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import (
+    Flask,
+    jsonify,
+    request,
+    render_template
+)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/database/<table_name>')
+def update_database(table_name):
+    pass
+
+def retrieve_data():
+    pass
+
+@app.route('/analysis/<int:year>', methods=['GET'])
+def employees_hired_dept(year):
+    data = summarize_year_dept(year)
+    return data
+    
+
+def _read_db(year):
+    pass
+
+def summarize_year_dept(year):
+    data = _read_db(year)
+
+app.run(port=5000, host='localhost', debug=True)
